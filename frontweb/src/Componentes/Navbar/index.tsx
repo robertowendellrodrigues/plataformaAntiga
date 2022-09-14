0ac7c257af3ad313e2b99d8ae13387bc3e983af6
Wendell <robertowendellrodrigues@gmail.com>
2022-09-13 21:22:31 -0300

    Estilização Navbar

--- frontweb/src/Componentes/Navbar/index.tsx
+++ frontweb/src/Componentes/Navbar/index.tsx
@@ -1,15 +1,28 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
     return (
         <>
 
-            <nav className="navbar navbar-expand-md bg-primary main-nav">
+            <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
                 <div className="container-fluid">
                     <a href="link" className="nav-logo-text">
                         <h4>Alphatec Sistemas</h4>
                     </a>
-                    <div className='collapse navbar-collapse'>
+                    <button 
+                    className='navbar-toggler'
+                    type='button'
+                    data-bs-toggle="collapse"
+                    data-bs-target="#site-navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    >
+                    <span className='navbar-toggler-icon'></span>
+                    </button>
+
+
+                    <div className='collapse navbar-collapse' id='site-navbar'>
                         <ul className='navbar-nav offset-md-2 main-menu'>
                             <li>
                                
@@ -27,9 +40,15 @@ const Navbar = () => {
                             <li>
                                 <a href="link">Relatório</a>
                             </li>
+                            <li>
+                                <a href="link" >login</a>
+                            </li>
+                            
                             
                         </ul>
+                        
                     </div>
+                    
                 </div>
             </nav>
 
